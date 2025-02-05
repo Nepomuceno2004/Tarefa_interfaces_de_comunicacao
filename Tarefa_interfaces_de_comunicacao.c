ece9061491081d4ca946d1af2c473a17c9f1246c
Matheus Nepomuceno Souza <mnepomuceno2004@gmail.com>
2025-02-04 23:07:04 -0300

    Update Tarefa_interfaces_de_comunicacao.c

--- Tarefa_interfaces_de_comunicacao.c
+++ Tarefa_interfaces_de_comunicacao.c
@@ -4,12 +4,18 @@
 #define led_pin_g 11
 #define led_pin_b 12
 #define led_pin_r 13
+#define button_a 5
+#define button_b 6
+
+static volatile uint32_t last_time = 0; // Armazena o tempo do último evento (em microssegundos)
+
+void gpio_irq_handler(uint gpio, uint32_t events);
+
 
 int main()
 {
     stdio_init_all();
 
-    // Configura os pinos dos LEDs como saída
     gpio_init(led_pin_r);
     gpio_set_dir(led_pin_r, GPIO_OUT);
     gpio_put(led_pin_r, 0); // Inicialmente desligado
@@ -22,7 +28,18 @@ int main()
     gpio_set_dir(led_pin_b, GPIO_OUT);
     gpio_put(led_pin_b, 0); // Inicialmente desligado
 
-    printf("RP2040 inicializado. Envie 'r', 'g' ou 'b' para alternar os LEDs.\n");
+    gpio_init(button_a);
+    gpio_set_dir(button_a, GPIO_IN);
+    gpio_pull_up(button_a);
+
+    gpio_init(button_b);
+    gpio_set_dir(button_b, GPIO_IN);
+    gpio_pull_up(button_b);
+
+    printf("RP2040 inicializado. Envie 'r', 'g' ou 'b' para alternar os LEDs ou aperte os botões 'A' ou 'B' .\n");
+
+    gpio_set_irq_enabled_with_callback(button_a, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(button_b, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
     while (true)
     {
@@ -30,7 +47,7 @@ int main()
         { // Certifica-se de que o USB está conectado
             char c;
             if (scanf("%c", &c) == 1)
-            { // Lê caractere da entrada padrão
+            {
                 printf("Recebido: '%c'\n", c);
 
                 switch (c)
@@ -56,4 +73,25 @@ int main()
     }
 
     return 0;
+}
+
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    if (current_time - last_time > 200000) // 200ms de debounce
+    {
+        last_time = current_time;
+
+        if (gpio == button_a)
+        {
+            gpio_put(led_pin_g, !gpio_get(led_pin_g));
+            printf("LED verde alternado!\n");
+        }
+        else if (gpio == button_b)
+        {
+            gpio_put(led_pin_b, !gpio_get(led_pin_b));
+            printf("LED azul alternado!\n");
+        }
+    }
 }
